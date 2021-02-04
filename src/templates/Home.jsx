d4d97753514b33232de7982acb7440b9f7997d99
Tatsuya <yokokura0413@gmail.com>
2021-02-05 05:42:59 +0900

    reducks

--- src/templates/Home.jsx
+++ src/templates/Home.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import { getUserId } from '../reducks/users/selectors'
+import { useSelector } from 'react-redux'
 
 const Home = () => {
+  const selector = useSelector(state => state)
+  const uid = getUserId(selector)
+
   return (
-    <h2>Home</h2>
+    <div>
+      <h2>Home</h2>
+      <p>{uid}</p>
+    </div>
   )
 }
 
--- src/templates/Login.jsx
+++ src/templates/Login.jsx
@@ -1,17 +1,17 @@
 import React from 'react'
-import { useDispatch, useSelector } from 'react-redux'
+import { useDispatch } from 'react-redux'
 import { push } from 'connected-react-router'
+import { signInAction } from '../reducks/users/actions'
 
 const Login = () => {
   const dispatch = useDispatch()
-  const selector = useSelector(state => state)
-
-  console.log(selector.router)
-
   return (
     <div>
       <h2>ログイン</h2>
-      <button onClick={() => dispatch(push('/'))}>
+      <button onClick={() => {
+        dispatch(signInAction({ uid: '0001', username: 'yokokura' }))
+        dispatch(push('/'))
+      }}>
         ログイン
       </button>
     </div>
